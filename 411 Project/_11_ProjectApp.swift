79bd19adecdfb1fba2e729af00e02e86dc4f4f44
minsuhk <xx.minsuhk.xx@gmail.com>
2025-10-01 22:39:27 -0700

    Initial Commit

---
+++ 411 Project/_11_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _11_ProjectApp.swift
+//  411 Project
+//
+//  Created by csuftitan on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct _11_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

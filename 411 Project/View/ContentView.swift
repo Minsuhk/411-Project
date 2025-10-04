4ce5c86973bcb29150131383542a952084d2e981
minsuhk <xx.minsuhk.xx@gmail.com>
2025-10-03 18:47:47 -0700

    -Setup the title -Setup the crude search bar

--- 411 Project/View/ContentView.swift
+++ 411 Project/View/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nearest_restroom = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            //testing how to push
+        //This is for the title
+        VStack(spacing: 4){
+            Text("Restroom Runner")
+                .font(.custom("Noteworthy-Light", size: 32, relativeTo: .largeTitle))
+                .padding(.top, 16)
+                //.background(Color.green)
+                .padding(.bottom, -13)
         }
-        .padding()
+        
+        //This is for the searchbar
+        NavigationStack {
+ 
+        }
+        .navigationTitle("Restroom Runner")
+        .navigationBarTitleDisplayMode(.large)
+        .searchable(text: $nearest_restroom,
+                    placement: .navigationBarDrawer(displayMode: .always),
+                    prompt: "Nearest Restroom")
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(.horizontal)
+        
+        
+        
+        
+        //End of body
     }
 }
 
